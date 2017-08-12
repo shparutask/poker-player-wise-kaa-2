ed829be7c51837d0cd40e27a588cafe1f1d8caa7
Mikhail.Kaduchka <Mikhail.Kaduchka@billing.ru>
2017-08-12 11:50:33 +0300

    equal ranks

--- player.py
+++ player.py
@@ -8,8 +8,19 @@ class Player:
         # sys.stdout(game_state)
 
         print(json.dumps(game_state))
+
         action_ = game_state['in_action']
-        return game_state['current_buy_in'] - game_state['players'][action_]['bet'] + game_state['minimum_raise']
+        mystatus = game_state['players'][action_]
+
+        firstCard = mystatus['hole_cards'][0]
+        secondCard = mystatus['hole_cards'][1]
+
+        allIn = game_state['current_buy_in'] - mystatus['bet'] + game_state['minimum_raise']
+
+        if firstCard['rank'] == secondCard['rank']:
+            return allIn
+        else:
+            return 0
 
     def showdown(self, game_state):
         pass
