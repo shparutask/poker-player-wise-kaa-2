33d428e497951c3d0e5b711069bb2c8743d91f23
Mikhail.Kaduchka <Mikhail.Kaduchka@billing.ru>
2017-08-12 11:00:12 +0300

    Logs

--- player.py
+++ player.py
@@ -1,8 +1,11 @@
+import sys
+
 
 class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
+        sys.stdout(game_state)
         return 10
 
     def showdown(self, game_state):
